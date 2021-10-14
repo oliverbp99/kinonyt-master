f81b725398fe5b9671e0b51d3f92b508ce24ab9c
oliverbp99 <oliverbpetersen99@gmail.com>
2021-10-14 12:59:11 +0200

    maven

--- src/main/java/edu/kea/kinoxp/config/SwaggerConfig.java
+++ src/main/java/edu/kea/kinoxp/config/SwaggerConfig.java
@@ -3,5 +3,6 @@ package edu.kea.kinoxp.config;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class SwaggerConfig {
+public class
+SwaggerConfig {
 }
--- src/main/java/edu/kea/kinoxp/controllers/GlobalController.java
+++ src/main/java/edu/kea/kinoxp/controllers/GlobalController.java
@@ -1,13 +1,8 @@
 package edu.kea.kinoxp.controllers;
-import edu.kea.kinoxp.models.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
-
-import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
 @ControllerAdvice("edu.kea.kinoxp.controllers")
 public class GlobalController {
